843cef9080f8ec61b27d8dea93be4f9b684980ca
Dumbdipshit <1211675@student.roc-nijmegen.nl>
2025-03-12 11:45:44 +0100

    added som functions
    cool

--- OvKaartVersie1/src/inchecken/Main.java
+++ OvKaartVersie1/src/inchecken/Main.java
@@ -2,8 +2,9 @@ package inchecken;
 
 public class Main{
 	public static void main(String[] args) {
-		OvKaart ovKaart1 = new OvKaart(0, 0, false, "none");
+		OvKaart ovKaart1 = new OvKaart(0, 0, false);
 		
+		ovKaart1.AddMoney(20.54);
 		ovKaart1.PrintInfo();
 
 	}
--- OvKaartVersie1/src/inchecken/OvKaart.java
+++ OvKaartVersie1/src/inchecken/OvKaart.java
@@ -5,18 +5,28 @@ public class OvKaart {
     private int euro = 0;
     private int cents = 0;
     private boolean checkedIn = false;
-    private String location = "none";
+    private String cardLocation = "none";
     
-	public OvKaart(int euro, int cents, boolean checkedIn, String location) {
+	public OvKaart(int euro, int cents, boolean checkedIn) {
 		this.euro = euro;
 		this.cents = cents;
 		this.checkedIn = checkedIn;
-		this.location = location;
 	}
 	
 	public void PrintInfo() {
 		System.out.println("Balance: " + this.euro + "." + this.cents);
 		System.out.println("Checked in: " + this.checkedIn);
-		System.out.println("Current location: " + this.location);
+		System.out.println("Current location: " + this.cardLocation);
+	}
+	
+	public void AddMoney(double money) {
+		int currencyEuro = (int) money;
+		this.euro = this.euro + currencyEuro;
+		int currencyCents = (int) (money * 100) - (currencyEuro * 100);
+		this.cents = this.cents + currencyCents;
+	}
+	
+	public void CheckIn() {
+		System.out.println("Still WIP");
 	}
 }
